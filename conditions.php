a5cb2afeab49568ded9ebb1e544c09cd037daa14
Teja Varma <tejavarma@Tejas-MacBook-Pro.local>
2017-12-05 14:56:50 +0530

    written a logic for greatest number

---
+++ conditions.php
@@ -0,0 +1,23 @@
+<?php
+$a=8;
+$b=8;
+$c=8;
+
+if($a>$b&&$a>$c){
+    echo "A is Greatest number";
+}elseif($b>$c){
+    if($b==$a){
+        echo "A B are equal and Great";
+    }else{
+        echo "B is Greatest number";
+    }
+}elseif($a==$b&&$b==$c){
+    echo "A B C are equal!";
+}elseif($b==$c){
+        echo "B C are equal and Great";
+}elseif($a==$c){
+        echo "A C are equal and Great";
+}else{
+    echo "C is great!";
+}
+?>

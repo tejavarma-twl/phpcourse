7898625cfee856127847405a134a8223325f72fc
Teja Varma <tejavarma@Tejas-MacBook-Pro.local>
2018-02-23 12:11:29 +0530

    added array files

--- arrays.php
+++ arrays.php
@@ -39,33 +39,50 @@
 // echo $myarray2['name2'];
 
 
-$myarray3 = array(   
-                array(
-                    array("manoj1","sai1","surya1"),
-                    array("manoj2","sai2","surya2"),
-                    array("manoj3","sai3","surya3")
-                ),
-                array(
-                    array("manoj4","sai4","surya4"),
-                    array("manoj5","sai5","surya5"),
-                    array("manoj6","sai6","surya6")
-                ),
-                array(
-                    array("manoj7","sai7","surya7"),
-                    array("manoj8","sai8","surya8"),
-                    array("manoj9","sai9","surya9")
-                )
-            );
+// $myarray3 = array(   
+//                 array(
+//                     array("manoj1","sai1","surya1"),
+//                     array("manoj2","sai2","surya2"),
+//                     array("manoj3","sai3","surya3")
+//                 ),
+//                 array(
+//                     array("manoj4","sai4","surya4"),
+//                     array("manoj5","sai5","surya5"),
+//                     array("manoj6","sai6","surya6")
+//                 ),
+//                 array(
+//                     array("manoj7","sai7","surya7"),
+//                     array("manoj8","sai8","surya8"),
+//                     array("manoj9","sai9","surya9")
+//                 )
+//             );
 
-foreach($myarray3 as $arr1){
-    foreach($arr1 as $arr2){
-        foreach($arr2 as $arr3){
-            echo $arr3;
-            echo "  ";
-        }
-        echo "<br>";
-    }
-    echo "<br>";
+// foreach($myarray3 as $arr1){
+//     foreach($arr1 as $arr2){
+//         foreach($arr2 as $arr3){
+//             echo $arr3;
+//             echo "  ";
+//         }
+//         echo "<br>";
+//     }
+//     echo "<br>";
+// }
+
+$firstname = "teja";
+$secondname = "varma";
+$percentage = 60;
+
+$resumeData = array(
+    'firstname'=>$firstname,
+    'secondname'=>$secondname,
+);
+
+if($percentage>60){
+    $resumeData['grade']="firstclass";
+}else{
+    $resumeData['grade']="";
 }
 
+print_r($resumeData);
+echo $resumeData['firstname'];
 ?>
